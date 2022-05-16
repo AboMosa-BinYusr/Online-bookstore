689a08fdd7591a33d93b024c4de8eda85955ec0c
AboMosa-BinYusr <100158596+AboMosa-BinYusr@users.noreply.github.com>
2022-05-16 15:00:43 +0200

    Adding Repositories

---
+++ BookStore1/Models/Repositories/MainCommentRepository.cs
@@ -0,0 +1,33 @@
+﻿using BookStore1.Data;
+using BookStore1.Models.Comments;
+using BookStore1.Models.Repositories.Interfaces;
+using BookStore1.ViewModels;
+
+namespace BookStore1.Models.Repositories
+{
+    public class MainCommentRepository : IMainCommentRepository
+    {
+        private readonly AppDataContext _database;
+
+        public MainCommentRepository(AppDataContext database)
+        {
+            _database = database;
+        }
+
+        public void AddMainComment(BookCommentViewModel bookCommentViewModel, string? userName)
+        {
+            if (bookCommentViewModel.CommentIntialId == 0)
+            {
+                _database.MainComments.Add(new MainComment
+                {
+                    CommentText = bookCommentViewModel.CommentText,
+                    BookId = bookCommentViewModel.Book.Id,
+                    UserName = userName,
+                    CreationDate = DateTime.Now,
+                });
+                _database.SaveChanges();
+            }
+
+        }
+    }
+}
